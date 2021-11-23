0aeaa90b45c793246bea719d00d9d8e292485810
DM0R3N0 <39966028+DM0R3N0@users.noreply.github.com>
2021-11-23 15:28:19 -0300

    Innitial Commit

---
+++ main.c
@@ -0,0 +1,61 @@
+//================================================================
+// Nome Do Arquivo: insertion.c
+//
+// Descricao: Implementacao do algoritmo de ordenacao por insercao
+//================================================================
+
+// Libs
+#include <stdio.h>
+#include <stdlib.h>
+
+// Define uma constante
+#define MAX 5
+
+// Prototipo da funcao de ordenacao
+void insertion_sort (int *a);
+
+int
+main (int argc, char **argv)
+{
+  int i, vetor[MAX];
+
+  // MAX ou 10 valores
+  for (i = 0; i < MAX; i++)
+    {
+      printf ("Digite um valor: ");
+      scanf ("%d", &vetor[i]);
+    }
+
+  printf ("\nOrdem inicial dos itens no array:\n");
+  for (i = 0; i < MAX; i++)
+    {
+      printf ("%4d", vetor[i]);
+    }
+
+  // Chamada da funcao que ordena os valores
+  insertion_sort (vetor);
+
+  // Imprime os valores ordenados
+  printf ("\nElementos do array em ordem crescente:\n");
+  for (i = 0; i < MAX; i++)
+    {
+      printf ("%4d", vetor[i]);
+    }
+}
+
+// Funcao de Ordenacao por Insercao
+void
+insertion_sort (int *a)
+{
+  int i, j, aux;
+
+  for (i = 1; i < MAX; i++)
+    {
+      aux = a[i];
+      for (j = i - 1; j >= 0 && aux < a[j]; j--)
+	{
+	  a[j + 1] = a[j];
+	}
+      a[j + 1] = aux;
+    }
+}
